6749bec4e930487e73053d5b16f5a46bf58a4ada
marh12hm <shark07729@sharkcoders.pt>
2025-11-06 20:02:07 +0000

    Add files via upload
    hiiiiiiiii

--- jump n catch/main/player.go
+++ jump n catch/main/player.go
@@ -22,6 +22,7 @@ embedded_components {
   "restitution: 0.5\n"
   "group: \"player\"\n"
   "mask: \"ground\"\n"
+  "mask: \"enemy\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
